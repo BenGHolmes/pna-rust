ed6f0c8d6cf98d508292590a9e18f60ec6ffd8c8
Ben Holmes <7benholmes@gmail.com>
2021-12-10 19:07:40 -0800

    Basic clap stuff

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,33 @@
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set the value of a given key")
+                .arg(Arg::with_name("KEY").required(true))
+                .arg(Arg::with_name("VALUE").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get the value of the given key")
+                .arg(Arg::with_name("KEY").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Remove the given key")
+                .arg(Arg::with_name("KEY").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", submatches) => println!("set {:?}", submatches),
+        ("get", submatches) => println!("get {:?}", submatches),
+        ("rm", submatches) => println!("rm {:?}", submatches),
+        (&_, _) => {}
+    }
 }
